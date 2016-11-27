899110b649b237acb1d6ee529948855f43f8e19a
baiyqukq <baiyqukq@163.com>
2016-11-27 22:25:22 +0800

    No comments

--- Client/Share.pas
+++ Client/Share.pas
@@ -1,94 +1,98 @@
 unit Share;
 
+{ Global assets definations }
 interface
 
 const
-  RUNLOGINCODE       = 0; //½øÈëÓÎÏ·×´Ì¬Âë,Ä¬ÈÏÎª0 ²âÊÔÎª 9
+	RUNLOGINCODE       = 0;			// Enter game status code, default 0, 9 for test
 
-  STDCLIENT          = 0;
-  RMCLIENT           = 99;
-  CLIENTTYPE         = RMCLIENT; //ÆÕÍ¨µÄÎª0 ,99 Îª¹ÜÀí¿Í»§¶Ë
+	STDCLIENT          = 0;
+	RMCLIENT           = 99;
+	CLIENTTYPE         = RMCLIENT;	// Normal is 0 , Manage Client is 99
 
-  RMCLIENTTITLE      = 'Diamond®';
+	RMCLIENTTITLE      = 'Diamond?';
 
 
-  DEBUG         = 0;
-  SWH800        = 0;
-  SWH1024       = 1;
-  SWH           = SWH800;
+	DEBUG         = 0;
+	SWH800        = 0;
+	SWH1024       = 1;
+	SWH           = SWH800;
 
-  CUSTOMLIBFILE = 0; //×Ô¶¨ÒåÍ¼¿âÎ»ÖÃ
+	CUSTOMLIBFILE = 0; // Custom library file position
 
 {$IF SWH = SWH800}
-   SCREENWIDTH = 800;
-   SCREENHEIGHT = 600;
+	SCREENWIDTH = 800;
+	SCREENHEIGHT = 600;
 {$ELSEIF SWH = SWH1024}
-   SCREENWIDTH = 1024;
-   SCREENHEIGHT = 768;
+	SCREENWIDTH = 1024;
+	SCREENHEIGHT = 768;
 {$IFEND}
 
-   MAPSURFACEWIDTH = SCREENWIDTH;
-   MAPSURFACEHEIGHT = SCREENHEIGHT- 155;
+	MAPSURFACEWIDTH = SCREENWIDTH;
+	MAPSURFACEHEIGHT = SCREENHEIGHT- 155;
 
-   WINLEFT = 60;
-   WINTOP  = 60;
-   WINRIGHT = SCREENWIDTH-60;
-   BOTTOMEDGE = SCREENHEIGHT-30;  // Bottom WINBOTTOM
+	WINLEFT = 60;
+	WINTOP  = 60;
+	WINRIGHT = SCREENWIDTH-60;
+	BOTTOMEDGE = SCREENHEIGHT-30;  // Bottom WINBOTTOM
 
-   MAPDIR             = 'Map\'; //µØÍ¼ÎÄ¼þËùÔÚÄ¿Â¼
-   CONFIGFILE         = 'Config\%s.ini';
+	//! [Assets files path
+	MAPDIR             = 'Map\'; // Map data files directory
+	CONFIGFILE         = 'Config\%s.ini';
 {$IF CUSTOMLIBFILE = 1}
-   EFFECTIMAGEDIR     = 'Graphics\Effect\';
-   MAINIMAGEFILE      = 'Graphics\FrmMain\Main.wil';
-   MAINIMAGEFILE2     = 'Graphics\FrmMain\Main2.wil';
-   MAINIMAGEFILE3     = 'Graphics\FrmMain\Main3.wil';
+	EFFECTIMAGEDIR     = 'Graphics\Effect\';
+	MAINIMAGEFILE      = 'Graphics\FrmMain\Main.wil';
+	MAINIMAGEFILE2     = 'Graphics\FrmMain\Main2.wil';
+	MAINIMAGEFILE3     = 'Graphics\FrmMain\Main3.wil';
 {$ELSE}
-   MAINIMAGEFILE      = 'Data\Prguse.wil';
-   MAINIMAGEFILE2     = 'Data\Prguse2.wil';
-   MAINIMAGEFILE3     = 'Data\Prguse3.wil';
-   EFFECTIMAGEDIR     = 'Data\';
+	MAINIMAGEFILE      = 'Data\Prguse.wil';
+	MAINIMAGEFILE2     = 'Data\Prguse2.wil';
+	MAINIMAGEFILE3     = 'Data\Prguse3.wil';
+	EFFECTIMAGEDIR     = 'Data\';
 {$IFEND}
 
-   CHRSELIMAGEFILE    = 'Data\ChrSel.wil';
-   MINMAPIMAGEFILE    = 'Data\mmap.wil';
-   TITLESIMAGEFILE    = 'Data\Tiles.wil';
-   SMLTITLESIMAGEFILE = 'Data\SmTiles.wil';
-   HUMWINGIMAGESFILE  = 'Data\HumEffect.wil';
-   MAGICONIMAGESFILE  = 'Data\MagIcon.wil';
-   HUMIMGIMAGESFILE   = 'Data\Hum.wil';
-   HAIRIMGIMAGESFILE  = 'Data\Hair.wil';
-   WEAPONIMAGESFILE   = 'Data\Weapon.wil';
-   NPCIMAGESFILE      = 'Data\Npc.wil';
-   MAGICIMAGESFILE    = 'Data\Magic.wil';
-   MAGIC2IMAGESFILE   = 'Data\Magic2.wil';
-   EVENTEFFECTIMAGESFILE = EFFECTIMAGEDIR + 'Event.wil';
-   BAGITEMIMAGESFILE   = 'Data\Items.wil';
-   STATEITEMIMAGESFILE = 'Data\StateItem.wil';
-   DNITEMIMAGESFILE    = 'Data\DnItems.wil';
-   OBJECTIMAGEFILE     = 'Data\Objects.wil';
-   OBJECTIMAGEFILE1    = 'Data\Objects%d.wil';
-   MONIMAGEFILE        = 'Data\Mon%d.wil';
-   DRAGONIMAGEFILE     = 'Data\Dragon.wil';
-   EFFECTIMAGEFILE     = 'Data\Effect.wil';
+	CHRSELIMAGEFILE    = 'Data\ChrSel.wil';
+	MINMAPIMAGEFILE    = 'Data\mmap.wil';
+	TITLESIMAGEFILE    = 'Data\Tiles.wil';
+	SMLTITLESIMAGEFILE = 'Data\SmTiles.wil';
+	HUMWINGIMAGESFILE  = 'Data\HumEffect.wil';
+	MAGICONIMAGESFILE  = 'Data\MagIcon.wil';
+	HUMIMGIMAGESFILE   = 'Data\Hum.wil';
+	HAIRIMGIMAGESFILE  = 'Data\Hair.wil';
+	WEAPONIMAGESFILE   = 'Data\Weapon.wil';
+	NPCIMAGESFILE      = 'Data\Npc.wil';
+	MAGICIMAGESFILE    = 'Data\Magic.wil';
+	MAGIC2IMAGESFILE   = 'Data\Magic2.wil';
+	EVENTEFFECTIMAGESFILE = EFFECTIMAGEDIR + 'Event.wil';
+	BAGITEMIMAGESFILE   = 'Data\Items.wil';
+	STATEITEMIMAGESFILE = 'Data\StateItem.wil';
+	DNITEMIMAGESFILE    = 'Data\DnItems.wil';
+	OBJECTIMAGEFILE     = 'Data\Objects.wil';
+	OBJECTIMAGEFILE1    = 'Data\Objects%d.wil';
+	MONIMAGEFILE        = 'Data\Mon%d.wil';
+	DRAGONIMAGEFILE     = 'Data\Dragon.wil';
+	EFFECTIMAGEFILE     = 'Data\Effect.wil';
 
-   MONIMAGEFILEEX      = 'Graphics\Monster\%d.wil';
-   MONPMFILE           = 'Graphics\Monster\%d.pm';
-   
-   {
-   MAXX = 40;
-   MAXY = 40;
-   }
-  MAXX = SCREENWIDTH div 20;
-  MAXY = SCREENWIDTH div 20;
+	MONIMAGEFILEEX      = 'Graphics\Monster\%d.wil';	// Monster Image File Ex
+	MONPMFILE           = 'Graphics\Monster\%d.pm';		// Monster PM File
+	//! ]
 
-  DEFAULTCURSOR = 0; //ÏµÍ³Ä¬ÈÏ¹â±ê
-  IMAGECURSOR   = 1; //Í¼ÐÎ¹â±ê
+	{
+	MAXX = 40;
+	MAXY = 40;
+	}
+	MAXX = SCREENWIDTH div 20;
+	MAXY = SCREENWIDTH div 20;
 
-  USECURSOR     = DEFAULTCURSOR; //Ê¹ÓÃÊ²Ã´ÀàÐÍµÄ¹â±ê
+	//! [Cursor
+	DEFAULTCURSOR = 0;					// System Default Cursor
+	IMAGECURSOR   = 1;					// Image Cursor
+	USECURSOR     = DEFAULTCURSOR;		// Use Cursor
+	//! ]
 
-  MAXBAGITEMCL = 52;
-  MAXFONT = 8;
-  ENEMYCOLOR = 69;
+	MAXBAGITEMCL = 52;		// Max Bag Item Count
+	MAXFONT = 8;			// Max Font
+	ENEMYCOLOR = 69;
 
 implementation
 
